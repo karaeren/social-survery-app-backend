5b9295456d5cfad194412d5e6b6a2414bc018ddb
Eren Kara <erenkyi0@gmail.com>
2022-01-03 23:12:14 +0300

    added base for results page

---
+++ public/js/results.js
@@ -0,0 +1,22 @@
+const resultElem = document.querySelector('#result');
+
+async function main() {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+
+  if (params && params.surveyId) {
+    try {
+      const response = await axios.get(
+        `/api/v1/survey/results/${params.surveyId}`
+      );
+
+      resultElem.innerText = JSON.stringify(response);
+    } catch (error) {
+      alert(error);
+    }
+  } else {
+    alert('Error, surveyId not found!');
+  }
+}
+
+window.onload = main;
