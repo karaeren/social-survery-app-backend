81732661dbe018434d768f2769fa987cd5a32905
Eren Kara <erenkyi0@gmail.com>
2022-03-21 23:59:04 +0300

    admin dashboard added missing styles

--- social-survey-app-admin-panel/src/main.js
+++ social-survey-app-admin-panel/src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue';
 import router from './router';
 
 import 'element-plus/theme-chalk/el-reset.css';
+import 'element-plus/es/components/message-box/style/css';
+import 'element-plus/es/components/message/style/css';
 
 const app = createApp(App);
 
