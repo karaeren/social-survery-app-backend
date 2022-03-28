02c2be5455b3269b51acde1bb88c16fab8e88658
Eren Kara <erenkyi0@gmail.com>
2022-03-28 12:35:03 +0300

    added pages for user management on admin panel

--- social-survey-app-admin-panel/src/composables/api/user.js
+++ social-survey-app-admin-panel/src/composables/api/user.js
@@ -1,6 +1,32 @@
 import axios from 'axios';
 
 export function useUserApi() {
+  async function getAllUsers(
+    accessToken,
+    name,
+    role,
+    sortBy,
+    limit = 1000,
+    page = 1
+  ) {
+    const response = await axios.get(
+      `https://socialsurveyapp.software/api/v1/users`,
+      {
+        params: {
+          ...(name && { name }),
+          ...(role && { role }),
+          ...(sortBy && { sortBy }),
+          limit,
+          page,
+        },
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+
+    const data = response.data;
+    return data;
+  }
+
   async function getUser(accessToken, userId) {
     const response = await axios.get(
       `https://socialsurveyapp.software/api/v1/users/${userId}`,
@@ -11,5 +37,17 @@ export function useUserApi() {
     return data;
   }
 
-  return { getUser };
+  async function deleteUser(accessToken, userId) {
+    const response = await axios.delete(
+      `https://socialsurveyapp.software/api/v1/users/${userId}`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+
+    const data = response.data;
+    return data;
+  }
+
+  return { getAllUsers, getUser, deleteUser };
 }
--- social-survey-app-admin-panel/src/views/users/ListUsers.vue
+++ social-survey-app-admin-panel/src/views/users/ListUsers.vue
@@ -1,18 +1,130 @@
 <template>
-  <div class="main">
-    <el-card class="box-card">
-      <template #header>
-        <div class="card-header">
-          <span>List Users</span>
-        </div>
-      </template>
-      todo
-    </el-card>
+  <div class="main centered-main">
+    <el-table
+      :data="filteredUsersData"
+      :default-sort="{ prop: 'rank' }"
+      height="100vh"
+      style="width: 100%"
+    >
+      <el-table-column fixed prop="role" label="Role" sortable width="90" />
+      <el-table-column prop="id" label="ID" sortable width="240" />
+      <el-table-column prop="name" label="name" sortable width="240" />
+      <el-table-column prop="email" label="Email" sortable width="240" />
+      <el-table-column prop="gender" label="Gender" width="120" />
+      <el-table-column prop="birthdate" label="Birthdate" width="180" />
+      <el-table-column
+        prop="surveyCount"
+        label="# of Submitted Surveys"
+        sortable
+        width="130"
+      />
+      <el-table-column fixed="right" align="right" min-width="240">
+        <template #header>
+          <el-input
+            v-model="search"
+            size="small"
+            placeholder="Type to search"
+          />
+        </template>
+        <template v-slot:default="table">
+          <el-button
+            type="danger"
+            size="small"
+            @click="deleteUserById(table.row.id)"
+          >
+            Delete
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
-<style>
-.main {
-  padding: 32px;
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+
+import { useAccountStore } from '@/stores/account';
+import { useUserApi } from '@/composables/api/user';
+
+import { ElMessageBox, ElMessage } from 'element-plus';
+
+const accountStore = useAccountStore(); // account store
+const { getAllUsers, deleteUser } = useUserApi(); // survey api
+
+const usersData = ref([]);
+const filteredUsersData = computed(() =>
+  usersData.value.filter(
+    (data) =>
+      !search.value ||
+      data.name.toLowerCase().includes(search.value.toLowerCase()) ||
+      data.email.toLowerCase().includes(search.value.toLowerCase())
+  )
+);
+
+const search = ref('');
+
+onMounted(async () => {
+  if (!accountStore.access.token) return;
+  await updateUsersList();
+  console.log(usersData.value);
+});
+
+async function updateUsersList() {
+  try {
+    const temp = await getAllUsers(accountStore.access.token);
+    temp.results.forEach((x) => {
+      if (x.birthdate)
+        x.birthdate = new Date(x.birthdate).toISOString().split('T')[0];
+
+      x.surveyCount = x.submittedSurveys.length;
+    });
+    usersData.value = temp.results;
+  } catch (e) {
+    console.error(e);
+    return ElMessageBox.alert(
+      'An unknown error happened while trying to retrieve users...',
+      'Error',
+      {
+        confirmButtonText: 'OK',
+      }
+    );
+  }
 }
-</style>
+
+function deleteUserById(id) {
+  ElMessageBox.confirm(
+    'Are you sure you want to delete this user? This is irreversible!',
+    'Warning',
+    {
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'Cancel',
+      type: 'warning',
+    }
+  )
+    .then(async () => {
+      try {
+        await deleteUser(accountStore.access.token, id);
+        ElMessage({
+          type: 'success',
+          message: 'Delete completed',
+        });
+        await updateUsersList();
+      } catch (e) {
+        console.error(e);
+        return ElMessageBox.alert(
+          "Couldn't delete user, an unknown error happened...",
+          'Error',
+          {
+            confirmButtonText: 'OK',
+          }
+        );
+      }
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: 'Delete canceled',
+      });
+    });
+}
+</script>
